eb70dfe45f3c8f47bdfe6967733117549d322d51
hsbulla <workjusel@gmail.com>
2023-04-26 19:34:22 +0700

    add_admin_panel

--- backend-container/game_project/game_project/settings.py
+++ backend-container/game_project/game_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'game_project']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ["../../frontend-container/HTML/S-HTML"],
-        'APP_DIRS': None,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
