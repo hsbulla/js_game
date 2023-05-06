39502f6da73201406871e0b852c95d178b4df917
hsbulla <workjusel@gmail.com>
2023-05-06 23:01:57 +0700

    #

--- backend-container/game_project/login_score/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from login_score.models import custom_game
-
-# Register your models here.
-
-@admin.register(custom_game)
-class custom_gameAdmin(admin.ModelAdmin):
-    pass
--- backend-container/game_project/login_score/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoginScoreConfig(AppConfig):
-    name = 'login_score'
