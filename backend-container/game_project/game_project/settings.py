a119fe251b7ed45db3ff8e62c2c4467a7d676676
hsbulla <workjusel@gmail.com>
2023-05-07 20:58:21 +0700

    global_redirect

--- backend-container/game_project/game_project/settings.py
+++ backend-container/game_project/game_project/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'login_score.middleware.InvalidURLMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
