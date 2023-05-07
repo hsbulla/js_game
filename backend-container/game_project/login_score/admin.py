5f0aef40e8873c8ec8916c6190dbe88185355de3
hsbulla <workjusel@gmail.com>
2023-05-07 20:26:29 +0700

    final_api

--- backend-container/game_project/login_score/admin.py
+++ backend-container/game_project/login_score/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from login_score.models import custom_game
+from login_score.models import player_info
+from login_score.models import player_shop
 
 # Register your models here.
 
-@admin.register(custom_game)
+@admin.register(custom_game , player_info, player_shop)
+
 class custom_gameAdmin(admin.ModelAdmin):
     pass
+
+class player_infoAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'coin', 'best_score']
+
+class player_shopAdmin(admin.ModelAdmin):
+    list_display = ('id','nickname', 'item1', 'item2', 'item3', 'item4', 'item5')
+
--- backend-container/game_project/login_score/views.py
+++ backend-container/game_project/login_score/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse, HttpResponseRedirect
-from login_score import utils
-from .models import custom_game
+from .models import custom_game, player_info, player_shop
 import random, json
+import sqlite3
 
 def login_score(request):
 
-    player_nicknames_score = []
-    for i in range(3):
-        random.shuffle(utils.player_nicknames)
-        nick = random.choice(utils.player_nicknames)
-        player_nicknames_score.append(nick)
-    player1_nickname, player2_nickname, player3_nickname = player_nicknames_score
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
+    cursor.execute("SELECT nickname, best_score FROM player_info ORDER BY best_score DESC LIMIT 3")
+    result_top = cursor.fetchall()
 
-    player_scores = []
-    for i in range(3):
-        score = random.randint(513, 730)
-        player_scores.append(score)
-    player_scores.sort()
-    player_scores.reverse()
-    player1_score, player2_score, player3_score = player_scores
+    player1_nickname = result_top[0][0]
+    player2_nickname = result_top[1][0]
+    player3_nickname = result_top[2][0]
+
+    player1_best_score = result_top[0][1]
+    player2_best_score = result_top[1][1]
+    player3_best_score = result_top[2][1]
 
     data = {"player_1": player1_nickname, 
             "player_2": player2_nickname, 
             "player_3": player3_nickname,
-            "score_1": player1_score,
-            "score_2": player2_score,
-            "score_3": player3_score}
+            "score_1": player1_best_score,
+            "score_2": player2_best_score,
+            "score_3": player3_best_score}
     
     return render(request, "base.html", context= data) 
 
@@ -53,18 +51,52 @@ def login_request(request):
         static_background = static_background_mappings.get(elements.backgrounds, "")
         static_obstacle = static_obstacle_mappings.get(elements.obstacle, "")
 
+
     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
     if is_ajax:
         if request.method == 'GET':
             nickname = request.GET.get('nikname_get', None)
-            coin_DB = 100
-            best_score_DB = 2
-            shop_DB = '1,1,0,0,0'
+            conn = sqlite3.connect('db.sqlite3')
+            cursor = conn.cursor()
+
+            coin_DB = 0
+            best_score_DB = 0
+            shop_DB = '1,0,0,0,0'
+
+            cursor.execute("SELECT coin, best_score FROM player_info WHERE nickname=?", (nickname,))
+            if cursor.fetchone() is None: 
+                player_info_to = player_info()
+                player_info_to.nickname = nickname
+                player_info_to.coin = 0
+                player_info_to.best_score = 0
+                player_info_to.save()
+            else: 
+                result = cursor.fetchone()
+                if result:
+                    coin_DB, best_score_DB = result
+
+            cursor.execute("SELECT item1, item2, item3, item4, item5 FROM player_shop WHERE nickname=?", (nickname,))
+            if cursor.fetchone() is None: 
+                player_shop_to = player_shop()
+                player_shop_to.nickname = nickname
+                player_shop_to.item1 = 1
+                player_shop_to.item2 = 0
+                player_shop_to.item3 = 0
+                player_shop_to.item4 = 0
+                player_shop_to.item5 = 0
+            else:
+                result_shop = cursor.fetchone()
+                if result_shop:
+                    item1, item2, item3, item4, item5 = result_shop
+                    shop_DB = [item1, item2, item3, item4, item5]
+                    shop_DB = ','.join(map(str, shop_DB))
+                    shop_DB = str(shop_DB)
+            conn.close()
             return JsonResponse({'coin_DB':  coin_DB,'best_score_DB': best_score_DB, 'shop_DB': shop_DB})
         return JsonResponse({'status': 'Invalid request'}, status=400)
     
     nickname = "player_"
-    number = str(random.randint(103, 138))
+    number = str(random.randint(1, 100000))
     nickname = nickname+number
     data_render = {"nickname": nickname, "static_platform": static_platform, "static_background": static_background, "static_obstacle": static_obstacle}    
 
@@ -72,7 +104,7 @@ def login_request(request):
         nickname_from_base = request.POST.get("nickname")
         if nickname_from_base == "":
             nickname_from_base = "player_"
-            number = str(random.randint(103, 138))
+            number = str(random.randint(1, 100000))
             nickname_from_base = nickname_from_base+number
         data_render = {"nickname": nickname_from_base, "static_platform": static_platform, "static_background": static_background, "static_obstacle": static_obstacle}
 
@@ -86,6 +118,46 @@ def api_response(request):
             coin_to_DB = int(coin_from_local_storage) - int(coin_DB)
             best_score_to_DB = data_api_to_DB['best_score_from_local_storage']
             shop_to_db = data_api_to_DB['shop_from_local_storage']
+            item1, item2, item3, item4, item5 = shop_to_db.split(',')
             nickname = data_api_to_DB['nickname_post']
-            print(nickname, coin_to_DB, best_score_to_DB, shop_to_db)
+
+            conn = sqlite3.connect('db.sqlite3')  
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM player_info WHERE nickname=?", (nickname,))
+            if cursor.fetchone() is None: 
+                player_info_to = player_info()
+                player_info_to.nickname = nickname
+                player_info_to.coin = coin_to_DB
+                player_info_to.best_score = best_score_to_DB
+                player_info_to.save()
+            else:   
+                cursor.execute("SELECT coin, best_score FROM player_info WHERE nickname=?", (nickname,))
+                result = cursor.fetchone()
+                if result:
+                    coin_now, best_score_delete = result
+                coin_sum = int(coin_now) + int(coin_to_DB)
+                cursor.execute("UPDATE player_info SET coin=?, best_score=? WHERE nickname=?", (coin_sum, best_score_to_DB, nickname))
+                conn.commit()
+            cursor.close()
+            conn.close()
+            
+            conn = sqlite3.connect('db.sqlite3') 
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM player_shop WHERE nickname=?", (nickname,))
+            if cursor.fetchone() is None: # записи нет, создаем новую
+                player_shop_to = player_shop()
+                player_shop_to.nickname = nickname
+                player_shop_to.item1 = item1
+                player_shop_to.item2 = item2
+                player_shop_to.item3 = item3
+                player_shop_to.item4 = item4
+                player_shop_to.item5 = item5
+                player_shop_to.save()
+            else:   # запись есть, обновляем значения в соответствующих столбцах
+                cursor.execute("UPDATE player_shop SET item1=?, item2=?, item3=?, item4=?, item5=?  WHERE nickname=?", (item1, item2, item3, item4, item5, nickname))
+                conn.commit()
+            cursor.close()
+            conn.close()
+
         return HttpResponseRedirect('/')
+
